da51235aaeb3aebe0101b50e1e28bdcf3a84f5c2
Laura Cole <lauracole1900@comcast.net>
2020-10-12 18:36:09 -0600

    portfolio page update, buttons don't work

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  // portfolio page onclick event
+  $(".siteBtn").on("click", function () {
+    // check button id
+    // match button id to appropriate site link
+    // open a new tab at that link
+    
+  })
+
+})
